b4664bd69f6213a664c73675153abb73ea8ae661
Alexei Romanov <neonzhlobin@tut.by>
2026-04-08 13:25:17 +0300

    Added docker configs

--- PatientService/src/PatientService.Api/Program.cs
+++ PatientService/src/PatientService.Api/Program.cs
@@ -1,14 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using PatientService.Persistence.Contexts;
 using PatientService.Persistence.Extensions;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -16,8 +30,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.MapControllers();
 
 app.Run();
+
+public partial class Program
+{
+}
